2296a3850d4ee3ab59ff8ff32088ff21e74c9001
bramilch <65772937+bramilch@users.noreply.github.com>
2024-02-17 23:38:29 +0900

    [Bronze I] Title: N번째 큰 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.py
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.py
@@ -8,6 +8,4 @@ case_dict = {}
 for i in range(T):
     case = list(map(int, input_func().split()))
     case_dict[i] = sorted(case, reverse = True)
-
-for j in range(T):
-    print(case_dict[j][2])
+    print(case_dict[i][2])
