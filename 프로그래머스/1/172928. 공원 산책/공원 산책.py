698d2de80d248dc1f09e2040066b41d43e894873
bramilch <65772937+bramilch@users.noreply.github.com>
2024-03-01 11:30:15 +0900

    [level 1] Title: 공원 산책, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,31 +1,31 @@
 def solution(park, routes):
-
-    x, y = 0, 0  # 시작점
-    w, h = len(park[0]), len(park)  # 가로, 세로
-    op = {"N":(-1,0), "S":(1,0), "W":(0,-1), "E":(0,1)}
+    answer = []  
+    direction = {'N': (-1, 0), 'S': (1, 0), 'E': (0, 1), 'W': (0, -1)}
     
-    # 시작점
-    for i in range(h):
-        for j in range(w):
-            if park[i][j]=="S":
-                x, y = i, j
+    # 시작 위치 찾기
+    for i in range(len(park)):
+        for j in range(len(park[0])):
+            if park[i][j] == 'S':
+                start_x, start_y = i, j
                 break
+    
+    for route in routes:
+        op, n = route[0], route[2]
+        current_x, current_y = start_x, start_y # 이동해 본 위치를 현재 위치로 갱신
         
-    # 좌표이동
-    for r in routes:
-        d, n = r.split(" ") # 방향, 이동횟수
-        dx, dy = x, y  # 현재위치
-        
+        # 이동해볼 위치 하나씩 확인하며 start_x, start_y 갱신
         for i in range(int(n)):
-            # 이동할 위치
-            nx = x + op[d][0]
-            ny = y + op[d][1]
-        
-            if 0<=nx<=h-1 and 0<=ny<=w-1 and (park[nx][ny]!="X"):
-                x, y = nx, ny
-                    
-            else:
-                x, y = dx, dy
-                break
+            move_x = start_x + direction[op][0]
+            move_y = start_y + direction[op][1]
 
-    return (x,y)
+            if 0 <= move_x <= len(park) -1 and 0 <= move_y <= len(park[0]) -1 and (park[move_x][move_y] != 'X'):
+                start_x, start_y = move_x, move_y 
+            
+            else:
+                start_x, start_y = current_x, current_y 
+                break
+    
+    # 최종 위치는 current가 아니라 start. 마지막 route는 start로 나옴
+    answer = [start_x, start_y]
+        
+    return answer
