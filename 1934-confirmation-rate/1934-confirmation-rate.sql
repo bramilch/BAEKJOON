ae11dfa1b101b618f8306ccb21380e0301b6683e
bramilch <65772937+bramilch@users.noreply.github.com>
2024-03-14 01:09:26 +0900

    Remove directly uploading LeetCode files and original files move to LeetCode Folder

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-SELECT S.user_id, ROUND(SUM(IF(C.action = 'confirmed', 1, 0)) / COUNT(*), 2) AS confirmation_rate
-FROM Signups AS S
-LEFT JOIN Confirmations AS C
-ON S.user_id = C.user_id
-GROUP BY s.user_id;
