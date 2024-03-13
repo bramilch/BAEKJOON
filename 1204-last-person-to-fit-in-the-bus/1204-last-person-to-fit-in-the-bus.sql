9345df39a91a285d9bc75877a69d0cfcb497af28
bramilch <bramilch5248@gmail.com>
2024-03-14 00:34:21 +0900

    Remove directly uploading LeetCode files and original files move to LeetCode Folder

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++
@@ -1,9 +0,0 @@
-# Write your MySQL query statement below
-SELECT A.person_name
-FROM Queue AS A
-JOIN (SELECT person_id, SUM(weight) OVER(ORDER BY turn) AS Total_Weight
-        FROM Queue) AS B
-ON A.person_id = B.person_id
-WHERE B.Total_Weight <= 1000
-ORDER BY B.Total_Weight DESC
-LIMIT 1;
