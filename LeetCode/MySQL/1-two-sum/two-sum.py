563cef79219e1b4ce3af8afa39972fe60376c80d
bramilch <65772937+bramilch@users.noreply.github.com>
2024-07-31 00:59:19 +0900

    Time: 63 ms (51.43%) | Memory: 18.1 MB (7.37%) - LeetSync

--- LeetCode/MySQL/1-two-sum/two-sum.py
+++ LeetCode/MySQL/1-two-sum/two-sum.py
@@ -1,7 +1,28 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # Exhaustive Search
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        
+        # Two Pointers
+        new_nums = [[v, i] for i, v in enumerate(nums)]
+        new_nums.sort(key=lambda x:x[0])
+        
+        l, r = 0, len(nums)-1
+        while l < r:
+            nums_sum = new_nums[l][0] + new_nums[r][0]
+            if nums_sum > target:
+                r -= 1
+            elif nums_sum < target:
+                l += 1
+            else:
+                return [new_nums[l][1], new_nums[r][1]]
+        
+
+
+
+        
